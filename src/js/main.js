cef0f9beedd5acb557ee249aff9b7e031eda12b3
Danil-Klimov <doink3690@gmail.com>
2020-08-05 13:35:40 +0300

    добавил блок our-projects

--- src/js/main.js
+++ src/js/main.js
@@ -9,5 +9,17 @@ $(document).ready(function () {
     slidesToScroll: 1,
     infinite: false
   });
+
+// our projects slider
+  $('.our-projects__body').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: false,
+    fade: true,
+    slide: '.our-projects__inner',
+    nextArrow: $(this).find('.arrow_next'),
+    prevArrow: $(this).find('.arrow_prev'),
+  });
+
   svg4everybody();
 });
