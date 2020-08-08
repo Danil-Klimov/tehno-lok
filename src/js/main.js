a9bca6f386c4bc95b096c6e784caf5491ab9eb24
Danil-Klimov <doink3690@gmail.com>
2020-08-08 13:12:04 +0300

    Переделал лайдер для types и reviews с помощью swiper

--- src/js/main.js
+++ src/js/main.js
@@ -26,14 +26,14 @@ $(document).ready(function () {
 
 // features slider
   const featuresSlider = new Swiper('.features__slider', {
-    direction: 'horizontal',
     loop: false,
     slidesPerView: 5,
+    centerInsufficientSlides: true,
+    watchOverflow: true
   });
 
 // our projects slider
   const ourProjectsSlider = new Swiper('.our-projects__slider', {
-    direction: 'horizontal',
     loop: false,
     slidesPerView: 1,
     effect: 'fade',
@@ -41,15 +41,14 @@ $(document).ready(function () {
       crossFade: true
     },
     navigation: {
-      nextEl: this.querySelector('.arrow_next'),
-      prevEl: this.querySelector('.arrow_prev'),
+      nextEl: '.our-projects__next',
+      prevEl: '.our-projects__prev',
     },
   });
 
   function our_project_img_slider() {
       $('.our-projects__img-slider').each(function () {
         let ourProjectsImgSlider = new Swiper(this, {
-          direction: 'horizontal',
           loop: false,
           slidesPerView: 1,
           effect: 'fade',
@@ -69,15 +68,14 @@ $(document).ready(function () {
   function our_projects_img_slider_w_nav () {
     $('.our-projects__img').each(function () {
       let ourProjectsImgSliderNav = new Swiper(this.querySelector('.our-projects__img-nav'), {
-        direction: 'horizontal',
         loop: false,
         slidesPerView: 4,
         watchSlidesProgress: true,
-        spaceBetween: 5
+        spaceBetween: 5,
+        centerInsufficientSlides: true
       });
 
       let ourProjectsImgSliderWNav = new Swiper(this.querySelector('.our-projects__img-slider-w-nav'), {
-        direction: 'horizontal',
         loop: false,
         slidesPerView: 1,
         effect: 'fade',
@@ -98,24 +96,37 @@ $(document).ready(function () {
   our_projects_img_slider_w_nav();
 
 // types slider
-  $('.types__slider').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    infinite: false,
+  const typesSlider = new Swiper('.types__slider', {
+    loop: false,
+    slidesPerView: 3,
+    spaceBetween: 30,
+    centerInsufficientSlides: true,
+    watchOverflow: true
   });
 
 // reviews slider
-  $('.reviews__slider').slick({
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    infinite: false,
-    nextArrow: '<button class="arrow arrow_md arrow_dark arrow_next" type="button">' +
-      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 11" fill="currentColor">' +
-      '<polygon points="9 5.5 0 0 0 11 9 5.5"/></svg></button>',
-    prevArrow: '<button class="arrow arrow_md arrow_dark arrow_prev" type="button">' +
-      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 11" fill="currentColor">' +
-      '<polygon points="0 5.5 9 0 9 11 0 5.5"/></svg></button>',
+  const reviewsSlider = new Swiper('.reviews__slider', {
+    loop: false,
+    slidesPerView: 'auto',
+    spaceBetween: 19,
+    centerInsufficientSlides: true,
+    watchOverflow: true,
+    navigation: {
+      nextEl: '.reviews__next',
+      prevEl: '.reviews__prev',
+    },
   });
+//   $('.reviews__slider').slick({
+//     slidesToShow: 5,
+//     slidesToScroll: 1,
+//     infinite: false,
+//     nextArrow: '<button class="arrow arrow_md arrow_dark arrow_next" type="button">' +
+//       '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 11" fill="currentColor">' +
+//       '<polygon points="9 5.5 0 0 0 11 9 5.5"/></svg></button>',
+//     prevArrow: '<button class="arrow arrow_md arrow_dark arrow_prev" type="button">' +
+//       '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 11" fill="currentColor">' +
+//       '<polygon points="0 5.5 9 0 9 11 0 5.5"/></svg></button>',
+//   });
 
 // gallery slider
   $('.gallery').slick({
