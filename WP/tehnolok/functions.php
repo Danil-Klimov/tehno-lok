f2b665604b28253a71f5a122e8940cbcf9b59581
Danil-Klimov <doink3690@gmail.com>
2020-08-14 20:42:15 +0300

    Добавил основные размеры миниатюр

--- WP/tehnolok/functions.php
+++ WP/tehnolok/functions.php
@@ -51,7 +51,29 @@ function js_variables() {
 // регистрация миниатюр
 add_action( 'after_setup_theme', 'tehnolok_register_images_thumbnails' );
 function tehnolok_register_images_thumbnails() {
+  add_image_size( '165x170', 165, 170 );
+  add_image_size( '210x270', 210, 270 );
+  add_image_size( '260x160', 260, 160 );
+  add_image_size( '273x185', 273, 185 );
+  add_image_size( '300x180', 300, 180 );
+  add_image_size( '325x160', 325, 160 );
+  add_image_size( '340x340', 340, 340 );
+  add_image_size( '345x210', 345, 210 );
   add_image_size( '345x540', 345, 540 );
+  add_image_size( '360x220', 360, 220 );
+  add_image_size( '360x250', 360, 250 );
+  add_image_size( '360x500', 360, 500 );
+  add_image_size( '390x250', 390, 250 );
+  add_image_size( '450x450', 450, 450 );
+  add_image_size( '480x400', 480, 400 );
+  add_image_size( '515x160', 515, 160 );
+  add_image_size( '515x290', 515, 290 );
+  add_image_size( '545x200', 545, 200 );
+  add_image_size( '620x450', 620, 450 );
+  add_image_size( '630x355', 630, 355 );
+  add_image_size( '695x390', 690, 390 );
+  add_image_size( '750x500', 750, 500 );
+  add_image_size( '935x525', 935, 525 );
 }
 
 // Регистрация типов постов
