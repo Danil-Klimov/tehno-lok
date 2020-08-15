7f8ce1d835bce705cb7f97e944017ed1d98de1a3
Danil-Klimov <doink3690@gmail.com>
2020-08-15 11:25:16 +0300

    Поправил блок about + поправил миниатюры

--- WP/tehnolok/blocks/about.php
+++ WP/tehnolok/blocks/about.php
@@ -5,24 +5,24 @@
         <div class="vertical-text"><?php the_sub_field( 'about_vertical-text' ); ?></div>
         <h2 class="title about__title"><?php the_sub_field( 'about_title' ); ?></h2>
         <?php $about_img = get_sub_field( 'about_img' ); ?>
-        <?php if ( $about_img ) : ?>
-          <img src="<?php echo esc_url( $about_img['url'] ); ?>" alt="<?php echo esc_attr( $about_img['alt'] ); ?>" />
+        <?php if( $about_img ) : ?>
+          <img src="<?php echo esc_url( $about_img[ 'url' ] ); ?>"
+               alt="<?php echo esc_attr( $about_img[ 'alt' ] ); ?>"/>
         <?php endif; ?>
       </div>
       <div class="about__content"><?php the_sub_field( 'about_content' ); ?></div>
     </div>
   </div>
-<!-- TODO когда будут страницы проектов, проверить этот блок -->
-  <?php if ( get_sub_field( 'about_projects-visible' ) == 1 ) : ?>
+  <?php if( get_sub_field( 'about_projects-visible' ) == 1 ) : ?>
     <div class="about__projects">
       <div class="about__projects-wrap">
         <?php $about_projects = get_sub_field( 'about_projects' ); ?>
-        <?php if ( $about_projects ) : ?>
-          <?php foreach ( $about_projects as $post ) : ?>
-            <?php setup_postdata ( $post ); ?>
+        <?php if( $about_projects ) : ?>
+          <?php foreach( $about_projects as $key => $post ) : ?>
+            <?php setup_postdata( $post ); ?>
             <a class="about__project"
                href="<?php the_permalink(); ?>"
-               style="background-image: linear-gradient(to bottom, rgba(23, 98, 175, 0.6) 0%, transparent 175px), url(<?php the_post_thumbnail_url( '345x540' ); ?>)">
+               style="background-image: linear-gradient(to <?= $key % 2 ? 'top' : 'bottom' ?>, rgba(23, 98, 175, 0.6) 0%, transparent 175px), url(<?php the_post_thumbnail_url( '345x540' ); ?>)">
               <div class="about__project-name">
                 <div class="about__project-title"><?php the_title(); ?></div>
                 <div class="arrow arrow_sm arrow_light about__project-arrow">
