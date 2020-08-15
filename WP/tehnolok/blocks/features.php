60f640cf24221322554a97af90e6695aa84e7f22
Danil-Klimov <doink3690@gmail.com>
2020-08-15 11:33:36 +0300

    Оформил блок features

--- WP/tehnolok/blocks/features.php
+++ WP/tehnolok/blocks/features.php
@@ -2,46 +2,40 @@
   <div class="container">
     <div class="row">
       <div class="features__header">
-        <div class="vertical-text">ПРЕИМУЩЕСТВА</div>
-        <h2 class="title features__title">ВЫГОДНО РАБОТАТЬ С НАМИ</h2>
-        <div class="features__subtitle">Потому что у нас:</div>
+        <div class="vertical-text"><?php the_sub_field( 'features__vertical-text' ); ?></div>
+        <h2 class="title features__title"><?php the_sub_field( 'features__title' ); ?></h2>
+        <?php if( get_sub_field( 'features__subtitle' ) ) : ?>
+          <div class="features__subtitle"><?php the_sub_field( 'features__subtitle' ); ?></div>
+        <?php endif; ?>
       </div>
-      <div class="features__container">
-        <div class="features__slider swiper-container">
-          <div class="swiper-wrapper">
-            <div class="features__item swiper-slide">
-              <div class="features__item-icon"><img src="img/features-icon-1.jpg" alt=""></div>
-              <div class="features__item-title">ОТЛИЧНАЯ ТЕХНОЛОГИЯ "ГИБКИЙ СЕНДВИЧ"</div>
-            </div>
-            <div class="features__item swiper-slide">
-              <div class="features__item-icon"><img src="img/features-icon-2.jpg" alt=""></div>
-              <div class="features__item-title">СОБСТВЕННОЕ ПРОИЗВОДСТВО В МОСКОВСКОЙ ОБЛАСТИ</div>
-            </div>
-            <div class="features__item swiper-slide">
-              <div class="features__item-icon"><img src="img/features-icon-3.jpg" alt=""></div>
-              <div class="features__item-title">ПРОЕКТИРОВАНИЕ В ПОЛНОМ СООТВЕТСТВИИ С НОРМАТИВАМИ РФ</div>
-            </div>
-            <div class="features__item swiper-slide">
-              <div class="features__item-icon"><img src="img/features-icon-4.jpg" alt=""></div>
-              <div class="features__item-title">МИНИМАЛЬНЫЕ СРОКИ ВОЗВЕДЕНИЯ ЗДАНИЯ</div>
-            </div>
-            <div class="features__item swiper-slide">
-              <div class="features__item-icon"><img src="img/features-icon-5.jpg" alt=""></div>
-              <div class="features__item-title">ОПТИМАЛЬНАЯ СТОИМОСТЬ ПОСТРОЙКИ</div>
-            </div>
+      <?php if ( have_rows( 'features__items' ) ) : ?>
+        <div class="features__container">
+          <div class="features__slider swiper-container">
+            <div class="swiper-wrapper">
+              <?php while ( have_rows( 'features__items' ) ) : the_row(); ?>
+                <?php $icon = get_sub_field( 'icon' ); ?>
+                <div class="features__item swiper-slide">
+                  <?php if ( $icon ) : ?>
+                    <div class="features__item-icon">
+                      <img src="<?= esc_url( $icon['url'] ); ?>" alt="<?= esc_attr( $icon['alt'] ); ?>" />
+                    </div>
+                  <?php endif; ?>
+                  <div class="features__item-title"><?php the_sub_field( 'text' ); ?></div>
+                </div>
+              <?php endwhile; ?>
           </div>
+          <button class="arrow arrow_sm arrow_dark arrow_prev features__prev" type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 11" fill="currentColor">
+              <polygon points="0 5.5 9 0 9 11 0 5.5"></polygon>
+            </svg>
+          </button>
+          <button class="arrow arrow_sm arrow_dark arrow_next features__next" type="button">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 11" fill="currentColor">
+              <polygon points="9 5.5 0 0 0 11 9 5.5"></polygon>
+            </svg>
+          </button>
         </div>
-        <button class="arrow arrow_sm arrow_dark arrow_prev features__prev" type="button">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 11" fill="currentColor">
-            <polygon points="0 5.5 9 0 9 11 0 5.5"></polygon>
-          </svg>
-        </button>
-        <button class="arrow arrow_sm arrow_dark arrow_next features__next" type="button">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 9 11" fill="currentColor">
-            <polygon points="9 5.5 0 0 0 11 9 5.5"></polygon>
-          </svg>
-        </button>
-      </div>
+      <?php endif; ?>
     </div>
   </div>
 </section>
