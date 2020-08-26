c77c9ce9fe5fa208d7c138a81a5bfd9b32ac7b6c
Danil-Klimov <doink3690@gmail.com>
2020-08-26 08:46:16 +0300

    Добавил возможность ставить текст, попап или ссылку в блок serv-menu

--- WP/tehnolok/blocks/serv-menu.php
+++ WP/tehnolok/blocks/serv-menu.php
@@ -4,16 +4,22 @@
       <div class="serv-menu__container">
         <?php if ( have_rows( 'serv-menu_blocks' ) ) : ?>
           <?php while ( have_rows( 'serv-menu_blocks' ) ) : the_row(); ?>
-            <?php $title = get_sub_field( 'title' ); ?>
+            <?php $title_link = get_sub_field( 'title-link' ); ?>
             <?php if( get_row_index() === 0 ) : ?>
               <div class="serv-menu__carcas" style="background-image: url(<?php the_sub_field( 'bg' ); ?>)">
-                <a class="serv-menu__title serv-menu__carcas-title"
-                   href="<?= esc_url( $title['url'] ); ?>"
-                  <?= get_sub_field( 'type-title' ) === 'popup' ? 'data-fancybox' : '' ?>><?= esc_html( $title['title'] ); ?></a>
+                <?php if( get_sub_field( 'type-title' ) === 'text' ) : ?>
+                  <div class="serv-menu__title serv-menu__carcas-title"><?php the_sub_field( 'title-text' ); ?></div>
+                <?php else : ?>
+                  <a class="serv-menu__title serv-menu__carcas-title"
+                     href="<?= esc_url( $title_link['url'] ); ?>"
+                    <?= get_sub_field( 'type-title' ) === 'popup' ? 'data-fancybox' : '' ?>><?= esc_html( $title_link['title'] ); ?></a>
+                <?php endif; ?>
                 <?php if ( have_rows( 'anchors' ) ) : ?>
                   <?php while ( have_rows( 'anchors' ) ) : the_row(); ?>
                     <?php $anchor = get_sub_field( 'anchor' ); ?>
-                    <?php if ( $anchor ) : ?>
+                    <?php if ( get_sub_field( 'anchor-type' ) === 'text' ) : ?>
+                      <div class="serv-menu__link"><?php the_sub_field( 'text' ); ?></div>
+                    <?php else : ?>
                       <a class="serv-menu__link"
                          href="<?= esc_url( $anchor['url'] ); ?>"
                         <?= get_sub_field( 'anchor-type' ) === 'popup' ? 'data-fancybox' : '' ?>>
@@ -26,14 +32,22 @@
             <?php else :?>
               <div class="serv-menu__coating" style="background-image: url(<?php the_sub_field( 'bg' ); ?>)">
                 <div class="serv-menu__coating-footer">
-                  <a class="serv-menu__title serv-menu__coating-title"
-                     href="<?= esc_url( $title['url'] ); ?>"
-                    <?= get_sub_field( 'type-title' ) === 'popup' ? 'data-fancybox' : '' ?>><?= esc_html( $title['title'] ); ?></a>
+                  <?php if( get_sub_field( 'type-title' ) === 'text' ) : ?>
+                    <div class="serv-menu__title serv-menu__coating-title"><?php the_sub_field( 'title-text' ); ?></div>
+                  <?php else : ?>
+                    <a class="serv-menu__title serv-menu__coating-title"
+                       href="<?= esc_url( $title_link['url'] ); ?>"
+                      <?= get_sub_field( 'type-title' ) === 'popup' ? 'data-fancybox' : '' ?>><?= esc_html( $title_link['title'] ); ?></a>
+                  <?php endif; ?>
                   <?php if ( have_rows( 'anchors' ) ) : ?>
                     <ul class="serv-menu__coating-list">
                       <?php while ( have_rows( 'anchors' ) ) : the_row(); ?>
                         <?php $anchor = get_sub_field( 'anchor' ); ?>
-                        <?php if ( $anchor ) : ?>
+                        <?php if ( get_sub_field( 'anchor-type' ) === 'text' ) : ?>
+                          <li>
+                            <div class="serv-menu__link"><?php the_sub_field( 'title-text' ); ?></div>
+                          </li>
+                        <?php else : ?>
                           <li>
                             <a class="serv-menu__link"
                                href="<?= esc_url( $anchor['url'] ); ?>"
